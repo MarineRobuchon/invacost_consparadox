f800c5579e10508c48c5662de94107c8055e03f8
Marine <Marine>
2020-08-14 11:05:27 +0200

    Update on originality scores for plants

--- scripts/originality_scores_plants.R
+++ scripts/originality_scores_plants.R
@@ -22,7 +22,8 @@ plant_tb_phylori <- distinctTree(plant_tree, method = "ED")
 write.csv2(plant_tb_phylori, "./outputs/plants_tree-based_phylori.csv") # save results
 
 ## distance-based phylogenetic originality
-plant_db_phylori <- distinctDis(plant_tree, method = "AV")
+d_plant_phylo <- dsimTree(plant_tree, type = "dissimilarity") # memory error
+plant_db_phylori <- distinctDis(d_plant_phylo, method = "AV")
 write.csv2(plant_db_phylori.med, "./outputs/plants_distance-based_phylori.csv") # save results
 
 
