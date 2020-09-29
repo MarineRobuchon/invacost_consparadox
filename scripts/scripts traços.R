a2108c39b5a83e5ab8c154494b2a57352fed6548
MarineRobuchon <robuchon@mnhn.fr>
2020-09-29 15:23:51 +0200

    Update on script to calculate functional originality for plants

--- scripts/scripts traços.R
+++ scripts/scripts traços.R
@@ -23,28 +23,21 @@ plant_tree
 #write.csv2(plant_tb_phylori,"plants_tree-based_phylori.csv") # save results
 
 ## load trait data
-df<-read.table("./data/traits.csv", sep=";", header=T, row.names=1 ) #  Careful of the path!
+df<-read.table("./data/traits_to_run.csv", sep=";", header=T, row.names=1 ) #  Careful of the path!
 df
 
 planttraits <- df
 dim(planttraits)
 
-planttraits <- na.omit(planttraits) # remove empty lines : this goes from 262445 species to 134032 by removing rows containing at least one NA. Necessary?
+planttraits <- na.omit(planttraits) # remove empty lines : this goes from 262445 species to 110403 by removing rows containing at least one NA. Necessary?
 rownames(planttraits) <- planttraits$species
 summary(planttraits)
-planttraits <- planttraits[, c("X1.1.1","X1.2.1", "X1.3.1", "X1.4.1", "X1.5.1")] # only keep trait values 
-
-
-
 
 # trait selection
-# plant_traits <- planttraits[, c("X1.1.1","X1.2.1", "X1.3.1", "X1.4.1", "X1.5.1")] # not necessary
-# trait transformation : already done (which one?)
-# trait correlation
-corPlot(planttraits, method = "pearson") # no pair of traits show correlation > 0.7 so we keep them all
+planttraits <- planttraits[, 1:6] # only keep trait values 
 # tree-based originality
 class(planttraits)
-# the variables are either quantitative or nominal, but not fuzzy!
-# try with quantitative
-dplant_traits <- dist.ktab(ktab.list.df(list(planttraits)), type = "Q") # calculation with Gower's general coefficient of distance
+# the variables are either quantitative (if transformed) or nominal, but not fuzzy!
+# try with nominal
+dplant_traits <- dist.ktab(ktab.list.df(list(planttraits)), type = "N") # calculation with Gower's general coefficient of distance
 
