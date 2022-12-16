dd6cd00ee959f02596f3a12d34a1b9450949c3eb
MarineRobuchon <robuchon@mnhn.fr>
2022-12-16 17:46:17 +0100

    Figures 3 and 4 updated, table of species both in Invacost and threatened built

--- scripts/05_analyses_figures_tables.R
+++ scripts/05_analyses_figures_tables.R
@@ -30,6 +30,9 @@ data_all$redlistCategory <- factor(data_all$redlistCategory, levels = c("least c
 # add a second column that will contribute to show them on two line on the graph
 data_all$species2lines <- gsub(" ", "\n", data_all$species)
 
+# transform oriFtree in numeric
+data_all$oriFtree <- as.numeric(data_all$oriFtree)
+
 ### Threat status and distinctiveness of the costliest invasive species in InvaCost ----
 ## identification of the costliest species by type of costs & taxon ----
 # damage
@@ -160,7 +163,8 @@ A_mammals <- ggplot(data = top5_mam_damage, aes(x = reorder(species2lines, oriPt
         axis.title.y = element_text(size = 9, face = "bold")) + 
   geom_hline(aes(yintercept = summary(mammals$oriPtree)[3]), color= "red", size = 0.5, alpha = 0.4) +
   geom_hline(aes(yintercept = summary(mammals$oriPtree)[5]), color= "red", size = 0.5, alpha = 0.4, linetype = "twodash") +
-  geom_text(aes(label = paste("rank", round(rank_oriPtree)), y = oriPtree + 0.3), size = 2, color = "black") +
+  geom_text(aes(label = paste("rank", round(rank_oriPtree)), y = oriPtree + 0.2*4.5), size = 2, color = "black") +
+  scale_y_continuous(limits = c(0, 6)) +
   coord_flip()
 A_mammals
 
@@ -179,7 +183,8 @@ C_birds <- ggplot(data = top5_bird_damage, aes(x = reorder(species2lines, oriPtr
         axis.title.y = element_text(size = 9, face = "bold")) + 
   geom_hline(aes(yintercept = summary(birds$oriPtree)[3]), color= "red", size = 0.5, alpha = 0.4) +
   geom_hline(aes(yintercept = summary(birds$oriPtree)[5]), color= "red", size = 0.5, alpha = 0.4, linetype = "twodash") +
-  geom_text(aes(label = paste("rank", round(rank_oriPtree)), y = oriPtree + 0.5), size = 2, color = "black") +
+  geom_text(aes(label = paste("rank", round(rank_oriPtree)), y = oriPtree + 0.2*7.5), size = 2, color = "black") +
+  scale_y_continuous(limits = c(0, 9.5)) +
   coord_flip()
 C_birds
 
@@ -198,9 +203,9 @@ E_plants <- ggplot(data = top5_plant_damage, aes(x = reorder(species2lines, oriP
         axis.title.y = element_text(size = 9, face = "bold")) + 
   geom_hline(aes(yintercept = summary(plants$oriPtree)[3]), color= "red", size = 0.5, alpha = 0.4) +
   geom_hline(aes(yintercept = summary(plants$oriPtree)[5]), color= "red", size = 0.5, alpha = 0.4, linetype = "twodash") +
-  geom_text(aes(label = paste("rank", round(rank_oriPtree)), y = oriPtree + 3), size = 2, color = "black") +
-  geom_text(aes(label = "phylogenetic distinctiveness not assessed", x = 5, y = 6), size = 2) +
-  scale_y_continuous(limits = c(0, summary(plants$oriPtree)[5] + 0.15)) +
+  geom_text(aes(label = paste("rank", round(rank_oriPtree)), y = oriPtree + 0.2*16), size = 2, color = "black") +
+  geom_text(aes(label = "phylogenetic distinctiveness not assessed", x = 5, y = 7), size = 2) +
+  scale_y_continuous(limits = c(0, 17)) +
   coord_flip()
 E_plants
 
@@ -217,7 +222,8 @@ B_mammals <- ggplot(data = top5_mam_management, aes(x = reorder(species2lines, o
         axis.title.y = element_text(size = 9, face = "bold")) + 
   geom_hline(aes(yintercept = summary(mammals$oriPtree)[3]), color= "red", size = 0.5, alpha = 0.4) +
   geom_hline(aes(yintercept = summary(mammals$oriPtree)[5]), color= "red", size = 0.5, alpha = 0.4, linetype = "twodash") +
-  geom_text(aes(label = paste("rank", round(rank_oriPtree)), y = oriPtree + 0.3), size = 2, color = "black") +
+  geom_text(aes(label = paste("rank", round(rank_oriPtree)), y = oriPtree + 0.2*7.5), size = 2, color = "black") +
+  scale_y_continuous(limits = c(0, 9.5)) +
   coord_flip()
 B_mammals
 
@@ -233,7 +239,8 @@ D_birds <- ggplot(data = top5_bird_management, aes(x = reorder(species2lines, or
         axis.title.y = element_text(size = 9, face = "bold")) + 
   geom_hline(aes(yintercept = summary(birds$oriPtree)[3]), color= "red", size = 0.5, alpha = 0.4) +
   geom_hline(aes(yintercept = summary(birds$oriPtree)[5]), color= "red", size = 0.5, alpha = 0.4, linetype = "twodash") +
-  geom_text(aes(label = paste("rank", round(rank_oriPtree)), y = oriPtree + 0.3), size = 2, color = "black") +
+  geom_text(aes(label = paste("rank", round(rank_oriPtree)), y = oriPtree + 0.2*7.5), size = 2, color = "black") +
+  scale_y_continuous(limits = c(0, 9.5)) +
   coord_flip()
 D_birds
 
@@ -249,9 +256,9 @@ F_plants <- ggplot(data = top5_plant_management, aes(x = reorder(species2lines, 
         axis.title.y = element_text(size = 9, face = "bold")) + 
   geom_hline(aes(yintercept = summary(plants$oriPtree)[3]), color= "red", size = 0.5, alpha = 0.4) +
   geom_hline(aes(yintercept = summary(plants$oriPtree)[5]), color= "red", size = 0.5, alpha = 0.4, linetype = "twodash") +
-  geom_text(aes(label = paste("rank", round(rank_oriPtree)), y = oriPtree + 2), size = 2, color = "black") +
-  geom_text(aes(label = "phylogenetic distinctiveness not assessed", x = 5, y = 6), size = 2) +
-  scale_y_continuous(limits = c(0, summary(plants$oriPtree)[5] + 0.15)) +
+  geom_text(aes(label = paste("rank", round(rank_oriPtree)), y = oriPtree + 0.2*16), size = 2, color = "black") +
+  geom_text(aes(label = "phylogenetic distinctiveness not assessed", x = 5, y = 7), size = 2) +
+  scale_y_continuous(limits = c(0, 17)) +
   coord_flip()
 F_plants
 
@@ -259,5 +266,173 @@ F_plants
 windows(6.85, 7)
 ggarrange(A_mammals + rremove("x.title"), B_mammals + rremove("x.title"), C_birds + rremove("x.title"), D_birds + rremove("x.title"), E_plants, F_plants, 
           ncol = 2, nrow = 3 , heights = c(1, 1, 1), common.legend = TRUE, legend = "bottom", align = "hv")
-dev.copy(png, file = paste0(getwd(), "/outputs/FIGURE1.png"), res = 600, height = 7, width = 6.85, units = "in")
+dev.copy(png, file = paste0(getwd(), "/outputs/FIGURE3.png"), res = 600, height = 7, width = 6.85, units = "in")
 dev.off()
+
+## figure showing functional distinctiveness scores of the TOP 5 costliest species (by type of cost and taxon) ----
+# damage
+summary(mammals$oriFtree)[3]  # median
+summary(mammals$oriFtree)[5]  # 3rd quantile
+
+mammals$rank_oriFtree <- rank(-mammals$oriFtree, na.last = "keep", ties.method = "average")
+top5_mam_damage <- merge(top5_mam_damage, mammals)
+
+A_mammals <- ggplot(data = top5_mam_damage, aes(x = reorder(species2lines, oriFtree), y = oriFtree)) + 
+  ggtitle("A  MAMMALS (indicated rank is out of 5,107 species)") + geom_point(stat="identity", size = 2) + theme_minimal() + 
+  labs(x = "DAMAGE\ntop 5 costliest species", y = "Functional distinctiveness") + 
+  theme(legend.title = element_text(color = "black", size = 7),legend.text = element_text(color = "black", size = 7),
+        plot.title = element_text(color="black", size = 9), axis.text.y = element_text(face="bold.italic", size = 7),
+        axis.text.x = element_text(face = "bold", color = "black", size = 9), axis.title.x = element_text(size = 9, face = "bold"), 
+        axis.title.y = element_text(size = 9, face = "bold")) + 
+  geom_hline(aes(yintercept = summary(mammals$oriFtree)[3]), color= "red", size = 0.5, alpha = 0.4) +
+  geom_hline(aes(yintercept = summary(mammals$oriFtree)[5]), color= "red", size = 0.5, alpha = 0.4, linetype = "twodash") +
+  geom_text(aes(label = paste("rank", round(rank_oriFtree)), y = oriFtree + 0.15*0.0115), size = 2, color = "black") +
+  scale_y_continuous(limits = c(0, 0.0125)) +
+  coord_flip()
+A_mammals
+
+
+summary(birds$oriFtree)[3]  # median
+summary(birds$oriFtree)[5]  # 3rd quantile
+birds$rank_oriFtree <- rank(-birds$oriFtree, na.last = "keep", ties.method = "average")
+top5_bird_damage <- merge(top5_bird_damage, birds)
+
+C_birds <- ggplot(data = top5_bird_damage, aes(x = reorder(species2lines, oriFtree), y = oriFtree)) + 
+  ggtitle("B  BIRDS (indicated rank is out of 9,758 species)") + geom_point(stat="identity", size = 2) + theme_minimal() + 
+  labs(x = "DAMAGE\ntop 5 costliest species", y = "Functional distinctiveness") + 
+  theme(legend.title = element_text(color = "black", size = 7),legend.text = element_text(color = "black", size = 7),
+        plot.title = element_text(color="black", size = 9), axis.text.y = element_text(face="bold.italic", size = 7),
+        axis.text.x = element_text(face = "bold", color = "black", size = 9), axis.title.x = element_text(size = 9, face = "bold"), 
+        axis.title.y = element_text(size = 9, face = "bold")) + 
+  geom_hline(aes(yintercept = summary(birds$oriFtree)[3]), color= "red", size = 0.5, alpha = 0.4) +
+  geom_hline(aes(yintercept = summary(birds$oriFtree)[5]), color= "red", size = 0.5, alpha = 0.4, linetype = "twodash") +
+  geom_text(aes(label = paste("rank", round(rank_oriFtree)), y = oriFtree + 0.2*0.04), size = 2, color = "black") +
+  scale_y_continuous(limits = c(0, 0.05)) +
+  coord_flip()
+C_birds
+
+
+summary(plants$oriFtree)[3]  # median
+summary(plants$oriFtree)[5]  # 3rd quantile
+plants$rank_oriFtree <- rank(-plants$oriFtree, na.last = "keep", ties.method = "average")
+top5_plant_damage <- merge(top5_plant_damage, plants)
+
+E_plants <- ggplot(data = top5_plant_damage, aes(x = reorder(species2lines, oriFtree), y = oriFtree)) + 
+  ggtitle("C PLANTS (indicated rank is out of 245,405 species)") + geom_point(stat="identity", size = 2) + theme_minimal() + 
+  labs(x = "DAMAGE\ntop 5 costliest species", y = "Functional distinctiveness") + 
+  theme(legend.title = element_text(color = "black", size = 7),legend.text = element_text(color = "black", size = 7),
+        plot.title = element_text(color="black", size = 9), axis.text.y = element_text(face="bold.italic", size = 7),
+        axis.text.x = element_text(face = "bold", color = "black", size = 9), axis.title.x = element_text(size = 9, face = "bold"), 
+        axis.title.y = element_text(size = 9, face = "bold")) + 
+  geom_hline(aes(yintercept = summary(plants$oriFtree)[3]), color= "red", size = 0.5, alpha = 0.4) +
+  geom_hline(aes(yintercept = summary(plants$oriFtree)[5]), color= "red", size = 0.5, alpha = 0.4, linetype = "twodash") +
+  geom_text(aes(label = paste("rank", round(rank_oriFtree)), y = oriFtree + 0.2*0.3), size = 2, color = "black") +
+  geom_text(aes(label = "Functional distinctiveness not assessed", x = 5, y = 0.15), size = 2) +
+  scale_y_continuous(limits = c(0, 0.4)) +
+  coord_flip()
+E_plants
+
+# management
+top5_mam_management <- merge(top5_mam_management, mammals)
+
+B_mammals <- ggplot(data = top5_mam_management, aes(x = reorder(species2lines, oriFtree), y = oriFtree)) + 
+  #ggtitle("B  MAMMALS") + 
+  geom_point(stat="identity", size = 2) + theme_minimal() + 
+  labs(x = "MANAGEMENT\ntop 5 costliest species", y = "Functional distinctiveness") + 
+  theme(legend.title = element_text(color = "black", size = 7),legend.text = element_text(color = "black", size = 7),
+        plot.title = element_text(color="black", size = 9), axis.text.y = element_text(face="bold.italic", size = 7),
+        axis.text.x = element_text(face = "bold", color = "black", size = 9), axis.title.x = element_text(size = 9, face = "bold"), 
+        axis.title.y = element_text(size = 9, face = "bold")) + 
+  geom_hline(aes(yintercept = summary(mammals$oriFtree)[3]), color= "red", size = 0.5, alpha = 0.4) +
+  geom_hline(aes(yintercept = summary(mammals$oriFtree)[5]), color= "red", size = 0.5, alpha = 0.4, linetype = "twodash") +
+  geom_text(aes(label = paste("rank", round(rank_oriFtree)), y = oriFtree + 0.2*0.019), size = 2, color = "black") +
+  scale_y_continuous(limits = c(0, 0.025)) +
+  coord_flip()
+B_mammals
+
+top5_bird_management <- merge(top5_bird_management, birds)
+
+D_birds <- ggplot(data = top5_bird_management, aes(x = reorder(species2lines, oriFtree), y = oriFtree)) + 
+  #ggtitle("D  BIRDS") + 
+  geom_point(stat="identity", size = 2) + theme_minimal() + 
+  labs(x = "MANAGEMENT\ntop 5 costliest species", y = "Functional distinctiveness") + 
+  theme(legend.title = element_text(color = "black", size = 7),legend.text = element_text(color = "black", size = 7),
+        plot.title = element_text(color="black", size = 9), axis.text.y = element_text(face="bold.italic", size = 7),
+        axis.text.x = element_text(face = "bold", color = "black", size = 9), axis.title.x = element_text(size = 9, face = "bold"), 
+        axis.title.y = element_text(size = 9, face = "bold")) + 
+  geom_hline(aes(yintercept = summary(birds$oriFtree)[3]), color= "red", size = 0.5, alpha = 0.4) +
+  geom_hline(aes(yintercept = summary(birds$oriFtree)[5]), color= "red", size = 0.5, alpha = 0.4, linetype = "twodash") +
+  geom_text(aes(label = paste("rank", round(rank_oriFtree)), y = oriFtree + 0.2*0.04), size = 2, color = "black") +
+  scale_y_continuous(limits = c(0, 0.05)) +
+  coord_flip()
+D_birds
+
+top5_plant_management <- merge(top5_plant_management, plants)
+
+F_plants <- ggplot(data = top5_plant_management, aes(x = reorder(species2lines, oriFtree), y = oriFtree)) + 
+  #ggtitle("F PLANTS") + 
+  geom_point(stat="identity", size = 2) + theme_minimal() + 
+  labs(x = "MANAGEMENT\ntop 5 costliest species", y = "Functional distinctiveness") + 
+  theme(legend.title = element_text(color = "black", size = 7),legend.text = element_text(color = "black", size = 7),
+        plot.title = element_text(color="black", size = 9), axis.text.y = element_text(face="bold.italic", size = 7),
+        axis.text.x = element_text(face = "bold", color = "black", size = 9), axis.title.x = element_text(size = 9, face = "bold"), 
+        axis.title.y = element_text(size = 9, face = "bold")) + 
+  geom_hline(aes(yintercept = summary(plants$oriFtree)[3]), color= "red", size = 0.5, alpha = 0.4) +
+  geom_hline(aes(yintercept = summary(plants$oriFtree)[5]), color= "red", size = 0.5, alpha = 0.4, linetype = "twodash") +
+  geom_text(aes(label = paste("rank", round(rank_oriFtree)), y = oriFtree + 0.2*0.4), size = 2, color = "black") +
+  scale_y_continuous(limits = c(0, 0.5)) +
+  coord_flip()
+F_plants
+
+# save figure
+windows(6.85, 7)
+ggarrange(A_mammals + rremove("x.title"), B_mammals + rremove("x.title"), C_birds + rremove("x.title"), D_birds + rremove("x.title"), E_plants, F_plants, 
+          ncol = 2, nrow = 3 , heights = c(1, 1, 1), common.legend = TRUE, legend = "bottom", align = "hv")
+dev.copy(png, file = paste0(getwd(), "/outputs/FIGURE4.png"), res = 600, height = 7, width = 6.85, units = "in")
+dev.off()
+### Presence in InvaCost and costs of the threatened and most distinctive species ----
+## identification of the threatened and most original species by taxon ----
+threatened <- data_all[which(data_all$redlistCategory%in%c("vulnerable", "endangered", "critically endangered")),]
+threatened_mammals <- threatened[which(threatened$taxon=="MAMMALS") ,]
+threatened_birds <- threatened[which(threatened$taxon=="BIRDS") ,]
+threatened_plants <- threatened[which(threatened$taxon== "PLANTS"),]
+
+TOP25PO_mammals <- mammals[which(mammals$oriPtree > summary(mammals$oriPtree)[5]),]
+TOP25PO_birds <- birds[which(birds$oriPtree > summary(birds$oriPtree)[5]),]
+TOP25PO_plants <- plants[which(plants$oriPtree > summary(plants$oriPtree)[5]),]
+
+TOP25FO_mammals <- mammals[which(mammals$oriFtree > summary(mammals$oriFtree)[5]),]
+TOP25FO_birds <- birds[which(birds$oriFtree > summary(birds$oriFtree)[5]),]
+TOP25FO_plants <- plants[which(plants$oriFtree > summary(plants$oriFtree)[5]),]
+
+TOP5PO_mammals <- mammals[which(mammals$oriPtree > quantile(mammals$oriPtree[!is.na(mammals$oriPtree)], 0.95)),]
+TOP5PO_birds <- birds[which(birds$oriPtree > quantile(birds$oriPtree[!is.na(birds$oriPtree)], 0.95)),]
+TOP5PO_plants <- plants[which(plants$oriPtree > quantile(plants$oriPtree[!is.na(plants$oriPtree)], 0.95)),]
+
+TOP5FO_mammals <- mammals[which(mammals$oriFtree > quantile(mammals$oriFtree[!is.na(mammals$oriFtree)], 0.95)),]
+TOP5FO_birds <- birds[which(birds$oriFtree > quantile(birds$oriFtree[!is.na(birds$oriFtree)], 0.95)),]
+TOP5FO_plants <- plants[which(plants$oriFtree > quantile(plants$oriFtree[!is.na(plants$oriFtree)], 0.95)),]
+
+## presence in InvaCost and costs of the threatened species ----
+threatened_mammals_inv <- threatened_mammals[-which(is.na(threatened_mammals$damage_cost|threatened_mammals$management_cost)),]
+threatened_mammals_inv[order(threatened_mammals_inv$redlistCategory, decreasing = TRUE),] 
+
+nrow(threatened_mammals_inv) # 5 mammal species are both threatened and have a cost in InvaCost
+nrow(threatened_mammals_inv[!is.na(threatened_mammals_inv$damage_cost),]) # 1 mammal species has a damage cost
+nrow(threatened_mammals_inv[!is.na(threatened_mammals_inv$management_cost),]) # 5 mammal species has a management cost
+
+threatened_birds_inv <- threatened_birds[-which(is.na(threatened_birds$damage_cost|threatened_birds$management_cost)),]
+threatened_birds_inv # 0 species of birds are threatened and in InvaCost
+
+threatened_plants_inv <- threatened_plants[-which(is.na(threatened_plants$damage_cost|threatened_plants$management_cost)),]
+threatened_plants_inv[order(threatened_plants_inv$redlistCategory, decreasing = TRUE),]
+nrow(threatened_plants_inv)# 5 species of plants are both threatened and have a cost in InvaCost 
+nrow(threatened_plants_inv[!is.na(threatened_plants_inv$damage_cost),]) # 1 plant species has a damage cost
+nrow(threatened_plants_inv[!is.na(threatened_plants_inv$management_cost),]) # 5 plant species has a management cost
+
+threatened_inv <- rbind(threatened_mammals_inv[order(threatened_mammals_inv$redlistCategory, decreasing = TRUE),], 
+                        threatened_plants_inv[order(threatened_plants_inv$redlistCategory, decreasing = TRUE),])
+colnames(threatened_inv)
+
+table_invacost_threatened <- threatened_inv[c("taxon", "species", "redlistCategory", "damage_cost", "management_cost")]
+write.csv2(table_invacost_threatened, paste0(getwd(), "/outputs/table_invacost_threatened.csv"))
